9f8c087f1f2e5cc0c7315a28e5ea82e085e2522c
pik <pik77905@gmail.com>
2025-10-22 00:56:46 +0300

    fix: vite.config.js

--- lab3/vite.config.js
+++ lab3/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	base: "/is-web/",
+	base: "./",
 	build: {
 		rollupOptions: {
 			input: {
